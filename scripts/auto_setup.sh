c033afb9abf1874d0afc28af6f352163df45cf76
Pasquale Lapomarda IV <user@neko.li>
2025-01-10 03:57:48 +0000

    fixed auto_setup.sh with correct read input and cleaned debug output

--- scripts/auto_setup.sh
+++ scripts/auto_setup.sh
@@ -3,19 +3,28 @@ set -e
 
 # variables
 COMPOSE_FILE="docker-compose.yml"
+PROJECT_ROOT=$(dirname "$0")/..
 
 # Ask user for tag when prompt for build image. If no tag is specified, then the default tag will be used.
-read -r -p "Enter the docker image tag for this build (or press Enter for default tag): " image_tag_name
+set +x   # Disable debug for read prompt
+IFS= read -n 1 -p "Enter the docker image tag for this build (or press Enter for default tag): " image_tag_name_input
+set -x  # Enable back for other commnads
 
-# Build and tag docker image with the tag provided by user, or using a default tag if empty string provided.
-./scripts/build_and_tag.sh "$image_tag_name"
-
-if [ ! -f .env ]; then #Check if the .env file exits.
-   echo ".env file does not exists, creating one with default values from .env-example."
-   cp .env-example .env #copy .env-example to .env for the first time.
+# use the user defined input, or use empty string as default if input is empty, this way no extra enter press needed.
+if [[ -z "$image_tag_name_input" ]]; then
+    image_tag_name=""
+else
+  image_tag_name="$image_tag_name_input"
 fi
 
-docker-compose up --build -d --remove-orphans # docker compose command.
+# Build and tag docker image with the tag provided by user, or using a default tag if empty string provided.
+"$PROJECT_ROOT"/scripts/build_and_tag.sh "$image_tag_name"
+
+if [ ! -f "$PROJECT_ROOT"/.env ]; then #Check if the .env file exits.
+   echo ".env file does not exists, creating one with default values from .env-example."
+   cp "$PROJECT_ROOT"/.env-example "$PROJECT_ROOT"/.env #copy .env-example to .env for the first time.
+fi
+
+docker-compose -f "$PROJECT_ROOT/$COMPOSE_FILE" up --build -d --remove-orphans # docker compose command.
 
 echo "Project setup complete." # Final message.
-
